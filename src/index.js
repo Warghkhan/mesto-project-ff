0ffeb3f6a3f8c9137ab66ec8e81f0ef82c355df0
WK <a.yambulatov@yandex.ru>
2024-07-26 02:33:06 +0400

    tst

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import './styles/index.css';
 import { initialCards } from './cards';
+import currentAvatarImage from './images/avatar.jpg';
 
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
 const addCardButton = document.querySelector(".profile__add-button");
 const cardContainer = document.querySelector(".places__list");
+const cardElement = cardTemplate.querySelector(".card");
 const cardImage = cardElement.querySelector(".card__image");
 // @todo: Функция создания карточки
+const avatarImage = new URL ('./images/avatar.jpg', import.meta.url);
+const avatar = {
+  name: "123",
+  link: currentAvatarImage
+}
+const avatarCircle = document.querySelector(".profile__image");
 
-function addCard(card, removeCard) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+function addCard(card,  removeCard ) {
+  cardElement.cloneNode(true);
   const deleteCardButton = cardElement.querySelector(".card__delete-button");
   deleteCardButton.addEventListener("click", () => removeCard(cardElement));
   cardElement.querySelector(".card__title").textContent = card.name;
@@ -30,5 +39,13 @@ function renderCards(cardArray) {
   });
 }
 renderCards(initialCards);
+
+function changeAvatar (avatar) {
+  avatarCircle.style.backgroundImage = avatar.link;
+}
+
+changeAvatar(avatar);
+
+
 
 // @todo: @todo: @todo:@todo:@todo:@todo:@todooooooooo: @dodododo: pinkpanther
