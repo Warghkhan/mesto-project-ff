6061e33d84ecf5bd7757bc8b73ff501521ff710d
WK <a.yambulatov@yandex.ru>
2024-07-29 01:32:16 +0400

    tst

--- src/modal.js
+++ src/modal.js
@@ -4,30 +4,45 @@ function openModal(modalWindow) {
   modalWindow.classList.remove("popup_is-animated");
   modalWindow.classList.add("popup_is-opened");
   const popupCloseButton = modalWindow.querySelector(".popup__close");
-  popupCloseButton.addEventListener("click", function () {
-    closeModal(modalWindow);
-  }, {once: true});
-
-  modalWindow.addEventListener("keydown", function (e) {
-    if (e.key === "Escape") {
+  popupCloseButton.addEventListener(
+    "click",
+    function () {
       closeModal(modalWindow);
-      console.log(e.key);
-    }
-  }, {once: true});
+    },
+    { once: true }
+  );
 
-  modalWindow.parentElement.addEventListener("keydown", function (e) {
-    if (e.key === "Escape") {
-      closeModal(modalWindow);
-      console.log(e.key);
-    }
-  }, {once: true});
+  modalWindow.addEventListener(
+    "keydown",
+    function (e) {
+      if (e.key === "Escape") {
+        closeModal(modalWindow);
+        //console.log(e.key);
+      }
+    },
+    { once: true }
+  );
 
-  modalWindow.addEventListener("click", function (e) {
-    if (e.target.classList.contains("popup_is-opened")) {
-      closeModal(modalWindow);
-    };
-  }, {once: true});
+  modalWindow.parentElement.addEventListener(
+    "keydown",
+    function (e) {
+      if (e.key === "Escape") {
+        closeModal(modalWindow);
+        //console.log(e.key);
+      }
+    },
+    { once: true }
+  );
 
+  modalWindow.addEventListener(
+    "click",
+    function (e) {
+      if (e.target.classList.contains("popup_is-opened")) {
+        closeModal(modalWindow);
+      }
+    },
+    { once: true }
+  );
 }
 
 function closeModal(modalWindow) {
